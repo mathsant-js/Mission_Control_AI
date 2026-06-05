0f0126fdb13b5ba785209fe975a5db35b636e3fe
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2026-06-05 18:50:24 -0300

    feat: completando alertas.py

---
+++ src/alertas.py
@@ -0,0 +1,30 @@
+def avaliar(dados):
+
+    alertas = []
+
+    if dados["latencia"] > 100:
+        alertas.append(
+            "CRÍTICO: Latência elevada."
+        )
+
+    if dados["throughput"] < 100:
+        alertas.append(
+            "ALERTA: Throughput abaixo do ideal."
+        )
+
+    if dados["saude_antena"] < 70:
+        alertas.append(
+            "CRÍTICO: Antena degradada."
+        )
+
+    if dados["beam_steering"] < 80:
+        alertas.append(
+            "ALERTA: Erro de apontamento."
+        )
+
+    if dados["temperatura"] > 75:
+        alertas.append(
+            "CRÍTICO: Superaquecimento do transponder."
+        )
+
+    return alertas
