a90c24b85429d5dec26f2b45117e229274d5d402
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2026-06-06 18:18:17 -0300

    test: adicionando modo demo para testar análise de tendência da IA
    - Caso o MODO_DEMO seja verdadeiro, data/cenarios.json é lido com dados pré-definidos de um cenário para testar análise de tendência da IA para uma missão que piorou
    - Caso o MODO_DEMO seja falso, os dados são gerados aleatoriamente dentro de um intervalo através da biblioteca random

--- src/telemetria.py
+++ src/telemetria.py
@@ -1,6 +1,23 @@
 import random
+import json
+
+MODO_DEMO = False
+
+indice_demo = 0
 
 def coletar():
+    global indice_demo
+    
+    if MODO_DEMO:
+        with open("data/cenarios.json", "r") as arquivo:
+            cenarios_demo = json.load(arquivo)
+
+        dados = cenarios_demo[indice_demo]
+
+        if indice_demo < len(cenarios_demo) - 1:
+            indice_demo += 1
+
+        return dados
 
     return {
         "latencia": random.randint(20, 150),
